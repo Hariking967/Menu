aafece0609a24a2a0c7adee090873340c4ee4e87
Sri Hari <harigallery9@gmail.com>
2025-09-04 06:59:12 +0530

    procedures doing

---
+++ src/modules/home/server/procedures.ts
@@ -0,0 +1,8 @@
+// import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+// import { db } from "@/db";
+// import { TRPCError } from "@trpc/server";
+// import {z} from 'zod';
+
+// export const menuRouter = createTRPCRouter({
+//   getMany: baseProcedure
+// })
